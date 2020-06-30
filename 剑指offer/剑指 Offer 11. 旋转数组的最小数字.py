592c997340decaed8e3e5788e5044a70759d1934
Ge-shi <geshi0905@gmail.com>
2020-06-30 13:55:34 +0800

    打卡

--- 剑指offer/剑指 Offer 11. 旋转数组的最小数字.py
+++ 剑指offer/剑指 Offer 11. 旋转数组的最小数字.py
@@ -1,3 +1,14 @@
 class Solution:
     def minArray(self, numbers: list) -> int:
-        return sorted(numbers)[0]
+        # return sorted(numbers)[0]
+        left = 0
+        right = len(numbers) - 1
+        while right > left:
+            mid = (right + left) // 2
+            if numbers[mid] > numbers[right]:
+                left = mid + 1
+            elif numbers[mid] < numbers[right]:
+                right = mid
+            else:
+                right -= 1
+        return numbers[left]
